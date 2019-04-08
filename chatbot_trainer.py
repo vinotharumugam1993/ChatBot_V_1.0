2183f1b0923f499968e9900b9544209de97d7e1e
vinotharumugam <vinotharumugam1993@gmail.com>
2019-04-09 04:21:38 +0530

    chatbot training script.

---
+++ chatbot_trainer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 00:15:13 2019
+
+@author: Vino
+"""
+
+try:
+    import argparse
+    from chatterbot.trainers import ListTrainer
+    from chatterbot import ChatBot
+    import os
+    import pickle
+except ImportError as e:
+    print(e)
+    print("Check wether the package {} is properly installed...".format(e))
+    
+class Chatbot_Trainer():
+    
+    def __init__(self):
+        
+        # construct the argument parser and parse the arguments
+        self.ap = argparse.ArgumentParser()
+        self.ap.add_argument("-d", "--dataset", required=True,
+        	help="path to input dataset")
+        self.ap.add_argument("-m", "--model", required=True,
+        	help="path to output trained model")
+        self.args = vars(self.ap.parse_args())
+        self.cb_obj = ChatBot('Bot')
+    
+    def train_model(self):
+        self.cb_obj.set_trainer(ListTrainer)
+        for files in os.listdir(self.args['dataset']):
+            data = open(self.args['dataset']+files,'r').readlines()
+            self.cb_obj.train(data)
+            
+    def save_model(self):
+        pickle.dump(self.cb_obj, open(self.args['model'], 'wb'))
+
+if __name__ == "__main__":
+    cb_training_obj = Chatbot_Trainer()
+    cb_training_obj.train_model()
+    cb_training_obj.save_model()
+    
+
